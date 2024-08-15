7f1feaffebad6fc0b5b73054f87a44ba034b2ecb
Heinz <aurelianob808@gmail.com>
2024-08-15 23:33:03 +0800

    finished proj1

--- gameoflife.c
+++ gameoflife.c
@@ -5,7 +5,7 @@
 ** DESCRIPTION: CS61C Fall 2020 Project 1
 **
 ** AUTHOR:      Justin Yokota - Starter Code
-**				YOUR NAME HERE
+**				SMS-Derfflinger
 **
 **
 ** DATE:        2020-08-23
@@ -17,12 +17,103 @@
 #include <inttypes.h>
 #include "imageloader.h"
 
+// The surrounding health state corresponds to the survival state
+const int alives[9] = {0, 0, 1, 1, 0, 0, 0, 0, 0};
+const int deads[9] = {0, 0, 0, 1, 0, 0, 0, 0, 0};
+
+typedef enum RGBchoice {
+    RED,
+    GREEN,
+    BLUE
+}RGBchoice;
+
+unsigned int getBit(uint8_t bytes, int bits) {
+    return (bytes >> bits) & 1;
+}
+
+void setBit(uint8_t *bytes, int bits, int value) {
+    if (value) {
+        *bytes = *bytes | (1 << bits);  // set nth bit to 1
+    } else {
+        *bytes = *bytes & (~(1 << bits)); // set nth bit to 0
+    }
+}
+
+uint8_t getRGB(Image *image, int row, int col, RGBchoice choice) {
+    uint8_t temp;
+    switch (choice)
+    {
+    case RED:
+        temp = image->image[row][col].R;
+        break;
+    case GREEN:
+        temp = image->image[row][col].G;
+        break;
+    case BLUE:
+        temp = image->image[row][col].G;
+        break;
+    default:
+        break;
+    }
+
+    return temp;
+}
+
+int getRealCoordinate(int origin, int offset, int total) {
+    int temp = origin + offset;
+    while (temp < 0){
+        temp += total;
+    }
+
+    return temp % total;
+}
+
+void setRGB(uint8_t *RGB, Image *image, int row, int col, RGBchoice choice) {
+    uint8_t tempRGB;
+
+    // uint8_t's 8 bits
+    for (int i = 0; i < 8; i++) {
+        int neighbors = 0;
+        for (int x = -1; x <= 1; x++) {
+            for (int y = -1; y <= 1; y++) {
+                int realRow = getRealCoordinate(row, y, image->rows);
+                int realCol = getRealCoordinate(col, x, image->cols);
+                tempRGB = getRGB(image, realRow, realCol, choice);
+                if (getBit(tempRGB, i) == 1) {
+                    neighbors++;
+                }
+            }
+        }
+
+        if (getBit(*RGB, i) == 1) {
+            setBit(RGB, i, alives[neighbors]);
+        } else {
+            setBit(RGB, i, deads[neighbors]);
+        }
+    }
+}
+
 //Determines what color the cell at the given row/col should be. This function allocates space for a new Color.
 //Note that you will need to read the eight neighbors of the cell in question. The grid "wraps", so we treat the top row as adjacent to the bottom row
 //and the left column as adjacent to the right column.
 Color *evaluateOneCell(Image *image, int row, int col, uint32_t rule)
 {
 	//YOUR CODE HERE
+    Color *color = (Color*)malloc(sizeof(Color));
+    if (color == NULL) {
+        perror("Error Allocating");
+        return NULL;
+    }
+
+    color->R = image->image[row][col].R;
+    color->G = image->image[row][col].G;
+    color->B = image->image[row][col].B;
+
+    setRGB(&color->R, image, row, col, RED);
+    setRGB(&color->G, image, row, col, GREEN);
+    setRGB(&color->B, image, row, col, BLUE);
+
+    return color;
 }
 
 //The main body of Life; given an image and a rule, computes one iteration of the Game of Life.
@@ -30,6 +121,38 @@ Color *evaluateOneCell(Image *image, int row, int col, uint32_t rule)
 Image *life(Image *image, uint32_t rule)
 {
 	//YOUR CODE HERE
+    Image *newImage = createNewImage(image->rows, image->cols);
+    if (newImage == NULL) {
+        return NULL;
+    }
+
+    for (int i = 0; i < image->rows; i++) {
+        for (int j = 0; j < image->cols; j++) {
+            Color *temp = evaluateOneCell(image, i, j, rule);
+            newImage->image[i][j] = *temp;
+            free(temp);
+        }
+    }
+
+    return newImage;
+}
+
+void processCLI(int argc, char **argv, char **filename, uint32_t *rule) 
+{
+	if (argc != 3) {
+		printf("usage: %s filename\n", argv[0]);
+		printf("filename is an ASCII PPM file (type P3) with maximum value 255.\n");
+		printf("rule is a hex number beginning with 0x; Life is 0x1808.\n");
+		exit(-1);
+	}
+	*filename = argv[1];
+	char *endPtr;
+	unsigned long hexValue = strtoul(argv[2], &endPtr, 16);
+	*rule = (uint32_t)hexValue;
+	if (!(*rule <= 0x3FFFF && *rule >= 0x00000)){
+		printf("rule is out of range\n");
+		exit(-1);
+	}
 }
 
 /*
@@ -50,4 +173,20 @@ You may find it useful to copy the code from steganography.c, to start.
 int main(int argc, char **argv)
 {
 	//YOUR CODE HERE
+    Image *image;
+	char *filename;
+	uint32_t rule;
+	processCLI(argc, argv, &filename, &rule);
+
+	image = readData(filename);
+    Image *newImage = life(image, rule);
+    if (image == NULL || newImage == NULL) {
+        return -1;
+    }
+
+    writeData(newImage);
+	freeImage(image);
+	freeImage(newImage);
+
+    return 0;
 }
