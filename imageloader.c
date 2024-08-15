30aaa71d9ac8e9065850e7d99b0e4c424fd57983
Heinz <aurelianob808@gmail.com>
2024-08-15 16:23:44 +0800

    finished readData function

--- imageloader.c
+++ imageloader.c
@@ -7,10 +7,10 @@
 ** AUTHOR:      Dan Garcia  -  University of California at Berkeley
 **              Copyright (C) Dan Garcia, 2020. All rights reserved.
 **              Justin Yokota - Starter Code
-**				YOUR NAME HERE
+**				SMS-Derfflinger
 **
 **
-** DATE:        2020-08-15
+** DATE:        2024-08-15
 **
 **************************************************************************/
 
@@ -26,6 +26,58 @@
 Image *readData(char *filename) 
 {
 	//YOUR CODE HERE
+    FILE *file = fopen("example.txt", "r");
+    if (file == NULL) {
+        perror("Error opening file.");
+        return NULL;
+    }
+
+    char format[3];
+    if (fscanf(file, "%2s", format) != 1 || strcmp(format, "P3") != 0) {
+        perror("Error reading format.");
+        fclose(file);
+        return NULL;
+    }
+
+    int cols, rows, max_val;
+    if (fscanf(file, "%d %d", &cols, &rows) != 2) {
+        perror("Error reading width and height.");
+        fclose(file);
+        return NULL;
+    }
+
+    if (fscanf(file, "%d", &max_val) != 1) {
+        perror("Error reading max_val.");
+        fclose(file);
+        return NULL;
+    }
+
+    Image *newImage = (Image *)malloc(sizeof(Image));
+    newImage->cols = cols;
+    newImage->rows = rows;
+    newImage->image = (Color **)calloc(rows, sizeof(Color *));
+    if (newImage->image == NULL){
+		perror("Error allocating.");
+        fclose(file);
+		return NULL;
+	}
+
+    for (int i = 0; i < rows; ++i){
+		newImage->image[i] = (Color*)calloc(newImage->cols, sizeof(Color));
+		if (newImage->image[i] == NULL){
+			perror("Error allocating.");
+            fclose(file);
+            return NULL;
+		}
+	}
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            fscanf(file, "%hhu %hhu %hhu", &(newImage->image[i][j].R), &(newImage->image[i][j].G), &(newImage->image[i][j].B));
+        }
+    }
+
+    return newImage;
 }
 
 //Given an image, prints to stdout (e.g. with printf) a .ppm P3 file with the image's data.
